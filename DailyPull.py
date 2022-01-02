c92ae2c83b6adbdb5a1ac8718a74611efcc93c48
anthonyrinaldi4 <anthony.rinaldi04@gmail.com>
2022-01-01 21:25:50 -0500

    Add success log

--- DailyPull.py
+++ DailyPull.py
@@ -19,10 +19,12 @@ if __name__ == "__main__":
 
         last_price = float(last_line.split(",")[-1].replace("\n", "").strip())
 
+        if price < last_price:
+            sendMsg(
+                f'Subject: NEW LOW PRICE\n{data["product"]} reached a new low price of ${price}.'
+            )
+
+        print(f'Completed day {datetime.now().strftime("%d/%m/%Y")} success')
+
     except Exception as e:
         print(f"ERROR: {e}")
-
-    if price < last_price:
-        sendMsg(
-            f'Subject: NEW LOW PRICE\n{data["product"]} reached a new low price of ${price}.'
-        )
