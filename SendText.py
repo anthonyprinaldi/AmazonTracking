2d6a0dc46c24f6ddf5b716bff3e742c87ef5a62b
anthonyrinaldi4 <anthony.rinaldi04@gmail.com>
2022-01-02 02:13:59 +0000

    Fix two factor authentication

--- SendText.py
+++ SendText.py
@@ -5,7 +5,7 @@ def sendMsg(body: str) -> None:
     smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
     smtpObj.ehlo()
     smtpObj.starttls()
-    smtpObj.login("anthony.rinaldi04@gmail.com", "Hockeyrocks4")
+    smtpObj.login("anthony.rinaldi04@gmail.com", "mxsrhrypjgkoposc")
     smtpObj.sendmail("anthony.rinaldi04@gmail.com", "6479914878@pcs.rogers.com", body)
     smtpObj.quit()
 
